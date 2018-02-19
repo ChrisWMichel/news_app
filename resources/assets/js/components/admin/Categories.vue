a4fdc0e171b8fd9debf99e6ddaa93a73e6c4e886
Chris Michel <christopherw.michel@gmail.com>
2018-02-19 09:30:49 -0600

    debugging

--- resources/assets/js/components/admin/Categories.vue
+++ resources/assets/js/components/admin/Categories.vue
@@ -102,8 +102,9 @@
                 })
             },
             addCategory(){
-            console.log('name' + this.add_name);
-            console.log('addCategory');
+                console.log('name' + this.add_name);
+                console.log('addCategory');
+
                 axios.post('api/add_category_name', {name: this.add_name, font: this.font})
                     .then(resp => {
                         this.getCategories();
